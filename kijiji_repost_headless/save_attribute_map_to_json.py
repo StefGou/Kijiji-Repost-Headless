0173b5f377894f5d13982dd137ec9bd15e029f98
StefGou <sgouin@gmail.com>
2017-05-11 13:50:54 -0400

    modified postAdAttributes dictionnary structure

--- kijiji_repost_headless/save_attribute_map_to_json.py
+++ kijiji_repost_headless/save_attribute_map_to_json.py
@@ -1,8 +1,6 @@
 import bs4, requests
-from KijijiApi import getToken
-
-kijiji_username = ""
-kijiji_password = ""
+from kijiji_api import get_token
+from kijiji_settings import kijiji_username, kijiji_password
 
 session = requests.session()
 
@@ -17,7 +15,7 @@ payload = {'emailOrNickname': kijiji_username,
             'password': kijiji_password,
             'rememberMe': 'true',
             '_rememberMe': 'on',
-            'ca.kijiji.xsrf.token': getToken(resp.text, 'ca.kijiji.xsrf.token'),
+            'ca.kijiji.xsrf.token': get_token(resp.text, 'ca.kijiji.xsrf.token'),
             'targetUrl': 'L3QtbG9naW4uaHRtbD90YXJnZXRVcmw9TDNRdGJHOW5hVzR1YUhSdGJEOTBZWEpuWlhSVmNtdzlUREpuZEZwWFVuUmlNalV3WWpJMGRGbFlTbXhaVXpoNFRucEJkMDFxUVhsWWJVMTZZbFZLU1dGVmJHdGtiVTVzVlcxa1VWSkZPV0ZVUmtWNlUyMWpPVkJSTFMxZVRITTBVMk5wVW5wbVRHRlFRVUZwTDNKSGNtVk9kejA5XnpvMnFzNmc2NWZlOWF1T1BKMmRybEE9PQ--'
             }
 resp = session.post(url, data = payload)
@@ -56,78 +54,76 @@ def getCategoryMap(session, branchCategories, isInitialRun):
     return {**leafCategory, **(getCategoryMap(session, newBranches, False))}
 
 # This is the dictionary that would need to be saved in a json file
-postAdAttributes = {}
+postAdAttributes = []
 """
-postAdAttributes is a dictionary that should look like this:
+postAdAttributes is a list of dictionaries that should look like this:
 
 {
-"category_id": 
+"category_id": "246",
+"category_name": "furniture > beds, mattresses",
+"attributes": [
     {
-    "name": "category_name",
-    
-    "attributes": 
+    "attribute_id": "forsaleby_s",
+    "attribute_name": "For Sale By:",
+    "attribute_options": 
         {
-        "attribute_id": 
-            {
-            "name": "attribute_name",
-            "options": 
-                {
-                "option1_value": "option1_name",
-                "option2_value": "option2_name",
-                "option3_value": "option3_name"
-                }
-            }
+        "ownr": "Owner",
+        "delr": "Dealer",
         }
     }
+    ]
 }
 """
 
 # for category_id, category_name in getCategoryMap(session, [], True).items():
 for category_id, category_name in test_categories.items():  # uncomment line 26 to use this
     print("Searching", category_name, "...\n")
-
-    postAdAttributes[category_id] = {}
-    postAdAttributes[category_id]['name'] = category_name
+    category_dict = {}
+    category_dict['category_id'] = category_id
+    category_dict['category_name'] = category_name
+    category_dict['attributes'] = []
 
     postingUrl="https://www.kijiji.ca/p-admarkt-post-ad.html?categoryId="+category_id
     newAdPage = session.get(postingUrl)
     newAdPageSoup = bs4.BeautifulSoup(newAdPage.text, 'html.parser')
     select_and_input = newAdPageSoup.find_all(['select', 'input'], {"name": lambda x: x and x.startswith('postAdForm.attributeMap')})
 
-    postAdAttributes[category_id]['attributes'] = {}
+
 
     for item in select_and_input:
 
         item_label = newAdPageSoup.find('label', {'for': item['id']})
 
+        attributes = {}
+        attributes['attribute_id'] = item['id']
+
         if item.name == "select":
-            postAdAttributes[category_id]['attributes'][item['id']] = {
-                'name' : item_label.text.replace('\n','').strip(),
-                'options' : {option['value'] : option.text for option in item.select('option') if option['value'] != ""}
-            }
+            attributes['attribute_name'] = item_label.text.replace('\n','').strip()
+            attributes['attribute_options'] = {option['value'] : option.text for option in item.select('option') if option['value'] != ""}
 
         elif item.name == "input":
+            attributes['attribute_name'] = item_label.text.replace('\n', '').strip()
 
             # if it's a text input, there's no options, the user must enter something manually
             if item['type'] == 'text':
-                postAdAttributes[category_id]['attributes'][item['id']] = {
-                    'name': item_label.text.replace('\n', '').strip(),
-                    'options': None
-                }
+                attributes['attribute_options'] = None
 
             else:
                 item_name = item.parent.text.replace('\n', '')
 
                 # if the attribute is already in the dictionary... ad the option to the options dict
-                if item['id'] in postAdAttributes[category_id]['attributes'].keys():
-                    postAdAttributes[category_id]['attributes'][item['id']]['options'][item['value']] = item_name
+                att = next((attribute for attribute in category_dict['attributes'] if attribute.get("attribute_id") == item['id']), None)
+                if att:
+                    att['attribute_options'][item['value']] = item_name
+                    continue
 
                 # else, create the attribute dict and the options dict
                 else:
-                    postAdAttributes[category_id]['attributes'][item['id']] = {
-                        'name': item_label.text.replace('\n', '').strip(),
-                        'options': {item['value']: item_name}
-                    }
+                    attributes['attribute_options'] = {item['value']: item_name}
+
+        category_dict['attributes'].append(attributes)
+
+    postAdAttributes.append(category_dict)
 
 # from pprint import pprint
 # pprint(postAdAttributes)
@@ -138,8 +134,8 @@ for category_id, category_name in test_categories.items():  # uncomment line 26 
 #     for attribute_id, attribute in category['attributes'].items():
 #         print("    ", attribute['name'], "(" + attribute_id + ")")
 #
-#         if attribute['options']:
-#             for option_id, option_name in attribute['options'].items():
+#         if attribute['attribute_options']:
+#             for option_id, option_name in attribute['attribute_options'].items():
 #                 print("        ", option_name, "("+option_id+")")
 #         else:
 #             print("         Needs user input")
@@ -149,5 +145,5 @@ for category_id, category_name in test_categories.items():  # uncomment line 26 
 #     print("\n==========================\n")
 
 import json
-with open('attributeMap.json', 'w') as fp:
+with open('kijiji_api.json', 'w') as fp:
     json.dump(postAdAttributes, fp)
